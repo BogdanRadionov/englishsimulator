d24980b1278ea84e7c8126a4b9b872d432b34d6d
Bogdan Radionov <radionov.bogdan@gmail.com>
2016-03-08 22:44:51 +0200

    Добавлено несколько функций для представления сообщений.

--- englishsimulator/msghelper.py
+++ englishsimulator/msghelper.py
@@ -22,3 +22,26 @@ def msg_multiline(l, startline='', endline='', numbers=False, startnum=1, delim=
 			print line
 	if endline:
 		print endline
+
+def str_inline(l, begining='', end='', delim=', ', silence=False):
+	if silence:
+		return ''
+	l = map(str, l)
+	return u'{0}{1}{2}'.format(begining, delim.join(l), end)
+
+def str_multiline(l, startline='', endline='', numbers=False, startnum=1, delim='-', silence=False):
+	if silence:
+		return ''
+	lines = []
+	l = map(str, l)
+	if startline:
+		lines.append(startline)
+	if numbers:
+		for num,  line in enumerate(l, startnum):
+			lines.append(u'{0} {1} {2}'.format(num, delim, line))
+	else:
+		for line in l:
+			lines.append(line)
+	if endline:
+		lines.append(endline)
+	return '\n'.join(lines)
